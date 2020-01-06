cda8c6598d33f4c1c9a304e342f1be8994df2865
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-01-06 16:29:48 -0600

    ShoesAppMVC Primer Commit

---
+++ ShoesApp2/Controllers/ZapatosController.cs
@@ -0,0 +1,36 @@
+﻿using ShoesApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoesApp2.Controllers
+{
+    public class ZapatosController : Controller
+    {
+        // GET: Zapatos
+        public ActionResult Index()
+        {
+            DataProductsEntities db = new DataProductsEntities();
+            //db.FASV1_GetAllProducts();
+
+            return View(db.FASV1_GetAllProducts());
+        }
+
+        public ActionResult Agregar()
+        {   
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Agregar(FASV1_GetAllProducts_Result p)
+        {
+            if (!ModelState.IsValid)            
+                return View();
+            
+
+            return View();
+        }
+
+    }
+}
